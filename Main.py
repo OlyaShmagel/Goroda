5f498cb86341b071169a9328037a8980409a576d
OlyaShmagel <olgashmagel1@gmail.com>
2025-08-01 09:42:51 +0300

    Добавлено округление координат

--- Main.py
+++ Main.py
@@ -6,13 +6,15 @@ def get_coordinates(city, key):
         geocoder = OpenCageGeocode(key)
         results = geocoder.geocode(city, language='ru')
         if results:
-            return results[0]['geometry']['lat'], results[0]['geometry']['lng']
+            lat = round(results[0]['geometry']['lat'], 2)
+            lon = round(results[0]['geometry']['lng'], 2)
+            return lat, lon
         else:
             return 'Город не найден'
     except Exception as e:
         return f'Возникла ошибка: {e}'
 
 key = '04aca0f7d31542fa87c7aec3b13f0354'
-city = 'London'
+city = 'Химки'
 coordinates = get_coordinates(city, key)
 print(f'Координаты города {city}: {coordinates}')
